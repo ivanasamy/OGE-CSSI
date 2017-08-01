4b6a37249cfdfde8666ea050f147fc7d095b36a1
ivanasamy <ivanaalardin@gmail.com>
2017-08-01 16:59:44 -0500

    ./

--- dear-oge/models.py
+++ dear-oge/models.py
@@ -4,3 +4,4 @@ class post_model(ndb.Model):
     text = ndb.StringProperty()
     time_stamp = ndb.DateTimeProperty(auto_now_add = True)
     user = ndb.StringProperty()
+    out = ndb.StringProperty()
