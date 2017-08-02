365e5d8846d26f33946d6cd3bc86088d0432456b
FlaggIgbo <theotherogeflagg@gmail.com>
2017-08-02 16:29:42 -0500

    e

--- dear-oge/OGE.py
+++ dear-oge/OGE.py
@@ -27,10 +27,10 @@ def oge_response(response):
 
     negative_responses = ["As Helen Keller once said, Keep your face to the sunshine and you cannot see a shadow.",
     "As said by the Dalai Lama, In order to carry a positive action we must develop here a positive vision.", "I am sorry about how you feel. A quote I really enjoy is one said by Mewtwo from Pokemon, I see now that the circumstances of one's birth are irrelevant. It is what you do with the gift of life that determines who you are",
-    "Your feelings are unfortunate, and I would suggest this quote by Martian Manhunter, The future is worth it. All the pain. All the tears. The future is worth the fight"]
+    "Your feelings are unfortunate, and I would suggest this quote by Martian Manhunter, The future is worth it. All the pain. All the tears. The future is worth the fight", "If you love someone, set them free. If they come back they're yours; if they don't they never were. - Richard Bach", "Truth is everybody is going to hurt you: you just gotta find the ones worth suffering for. - Bob Marley", "Never love someone who treats you like you are ordinary - Oscar Wilde"]
 
-    neutral_responses = ["A quote I believe applies to a lot of what we do in life is 'If something is too hard, either you are not doing it right or it is not worth doing'", "The opportunity for success is there, you just need to acknowledge its presence and grasp it" , "In anything you do, you are only as good as you think you are."]
-#hello
+    neutral_responses = ["A quote I believe applies to a lot of what we do in life is 'If something is too hard, either you are not doing it right or it is not worth doing'", "The opportunity for success is there, you just need to acknowledge its presence and grasp it" , "In anything you do, you are only as good as you think you are.", "Anything is possible when you have inner peace - Master Shifu(Kung Fu Panda)","To laugh at yourself is to love yourself - Mickey Mouse", "Sucking at something is the first step to becoming sorta good at something - Jake from Adventure Time"]
+#
     positive_responses = ["Congratulations! You seem to be happy and that’s great! Life is short so enjoying it to its fullest is something special!", "Your happiness is something to be admired! You’re so lucky that things are working out in your life and I hope that things continue to work out!", "That’s wonderful! As an experienced giver of relationship advice, it is always great to hear about people whose lives are filled with joy", "I’m so happy for you! Your joy brings me joy as well!", "Good for you! May happiness forever be in your life!"]
     if sentiment > 0 and sentiment <0.3:
         return negative_responses[random.randint(0, len(negative_responses)-1)]
